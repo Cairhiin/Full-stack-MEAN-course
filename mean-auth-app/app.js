6b6455ff1e1ec421a6add512a2bd66260f0f4b29
Frank van de Voorde <daemonaz@kymp.net>
2023-01-20 19:50:24 +0200

    Add express server set up for the MEAN stack tutorial

---
+++ mean-auth-app/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const users = require('./routes/users');
+
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+	console.log(`Connected to the database ${config.database}!`);
+});
+mongoose.connection.on('error', (err) => {
+	console.log(`Databse error: ${err}!`);
+});
+
+const app = express();
+
+const port = process.env.PORT | 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+	res.send('Invalid Endpoint');
+})
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}!`);
+});
