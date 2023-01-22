81ac4cc80b6dad77df38fdda55bd80d9a7a0128e
Frank van de Voorde <daemonaz@kymp.net>
2023-01-22 16:30:07 +0200

    Refactor dir structure

---
+++ coursework/typescript-crash-course/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'Steve'],
+    [3, 'Jill']
+];
+// Union
+let pid = 22;
