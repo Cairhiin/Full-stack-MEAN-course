53964dfb09be86a3b7e85c8f2936b8f932a41e6e
Frank van de Voorde <daemonaz@kymp.net>
2023-02-01 20:50:50 +0200

    Add styling and add functionality to delete user

--- fullstack-MEAN-project/angular-src/src/app/components/delete-book/delete-book.component.ts
+++ fullstack-MEAN-project/angular-src/src/app/components/delete-book/delete-book.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Book } from '../../book';
 
 @Component({
   selector: 'app-delete-book',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-book.component.scss']
 })
 export class DeleteBookComponent {
+  @Input() book?: Book;
+  @Output() onDeleteBook = new EventEmitter<string>();
+  @Output() onCancel = new EventEmitter();
 
+  goBack(): void {
+    this.onCancel.emit();
+  }
+
+  deleteBook(): void {
+    this.book && this.onDeleteBook.emit(this.book.id);
+  }
 }
