d4a757fef16c2e255cf14dd5cadb630cebb2eee9
Frank van de Voorde <daemonaz@kymp.net>
2023-02-01 15:21:27 +0200

    Add documentation

--- fullstack-MEAN-project/angular-src/src/app/components/book-detail/book-detail.component.ts
+++ fullstack-MEAN-project/angular-src/src/app/components/book-detail/book-detail.component.ts
@@ -42,6 +42,10 @@ export class BookDetailComponent implements OnInit {
     this.location.back();
   }
 
+  /*
+  Get the number of times the book has been rated
+  by looping over the keys and summing their values
+  */
   getNumberOfRatings(): number {
     let count = 0;
 
@@ -53,14 +57,17 @@ export class BookDetailComponent implements OnInit {
     return count;
   }
 
+  /* 
+  Get the rating as a percentage score and round to two decimals
+  */
   getRatingScorePercentage(rating: number): number {
     return Math.round(rating / this.getNumberOfRatings() * 100);
   }
 
   /* 
-    Check if the user has already rated the book and
-    if true assign the rating and set hasBeenRated to true
-    to avoid the user from rating the book again
+  Check if the user has already rated the book and
+  if true assign the rating and set hasBeenRated to true
+  to avoid the user from rating the book again
   */
   checkIfBookHasBeenRated(): boolean {
     if (!this.user || !this.book) return false;
@@ -78,9 +85,9 @@ export class BookDetailComponent implements OnInit {
   }
 
   /* 
-    Receive the rating when the user clicks the rating component
-    and update both the book and user document in the database
-    then update the current book and user with the return value
+  Receive the rating when the user clicks the rating component
+  and update both the book and user document in the database
+  then update the current book and user with the return value
   */
   updateRating(value: number): void {
     if (this.user && this.book) {
@@ -96,6 +103,10 @@ export class BookDetailComponent implements OnInit {
     }
   }
 
+  /* 
+  Check if the reader has marked this book as
+  currently being read by them
+  */
   checkIfUserIsReading(): boolean {
     if (this.user && this.book && this.user.reading) {
         return this.user.reading.ISBN === this.book.ISBN;
